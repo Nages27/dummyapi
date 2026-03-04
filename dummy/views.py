38637746150364d619a252f24477c81b7bcc4ef5
Nageswari N <nageswari@example.com>
2026-03-04 15:21:50 +0530

    Updated the view

--- dummy/views.py
+++ dummy/views.py
@@ -10,6 +10,8 @@ from rest_framework import status
 
 
 class to(APIView):
+
+    renderer_classes = [JSONRenderer]  
     def post(self,request):
         serial=serializeduser(data=request.data)
         if serial.is_valid():
